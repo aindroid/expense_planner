e9e4ff0e36e564c6b637d93ac14236e872a3c1a4
ainhoa.moreno <ainhoa.moreno@infinityworks.com>
2022-06-22 11:03:36 +0100

    Add a empty list image

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,15 +12,31 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
-            itemBuilder: (ctx, index) {
-              final transaction = transactions[index];
-              return TransactionItem(
-                  amount: transaction.amount,
-                  date: transaction.date,
-                  title: transaction.title);
-            },
-            itemCount: transactions.length));
+        child: transactions.isEmpty
+            ? Column(
+                children: [
+                  Text(
+                    'No transactions added yet',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                      height: 200,
+                      child: Image.asset('assets/images/waiting.png',
+                          fit: BoxFit.cover))
+                ],
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  final transaction = transactions[index];
+                  return TransactionItem(
+                      amount: transaction.amount,
+                      date: transaction.date,
+                      title: transaction.title);
+                },
+                itemCount: transactions.length));
     // children: [
     //   ...transactions.map(
     //     (transaction) {
